ff2b24304d13a8d6f4492bbf1a09b7e7a41dfe81
aletanus <alessandro@altadesign.com.br>
2023-03-08 18:55:55 -0300

    Updated version. Just added some small corrections to a middleware.

--- src/middlewares/users/checkIfUserIsAdmin.ts
+++ src/middlewares/users/checkIfUserIsAdmin.ts
@@ -4,18 +4,13 @@ import { AppError } from "../../errors"
 
 export const checkIfUserIsAdminMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
     
-  // const authtenticatedAdmin = req.user.admin
-  // if (authtenticatedAdmin === false) {
-  //   throw new AppError("Insufficient permission", 403)
-  // }
-
   const authtenticatedAdmin = req.user.admin
   const userId = req.user.id
   const paramsUserId = Number(req.params.id)
   if (authtenticatedAdmin === true) {
     return next()
   } else {
-    if ((req.method === "PATCH" || req.method === "DELETE") && userId === paramsUserId){
+    if ((req.method === "PATCH") && userId === paramsUserId){
       return next()
     }
     throw new AppError("Insufficient permission", 403)
